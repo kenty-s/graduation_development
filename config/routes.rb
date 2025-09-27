19fcd5929ea92d8ebbdcb34642a784e67e68b30b
kenty <kenta.kgac@gmail.com>
2025-09-27 23:43:26 +0900

    66_favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get "home/index"
+  post 'search_foods', to: 'foods#search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
