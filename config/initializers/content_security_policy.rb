dbd7436702cae8779640ef6de2b4f065f5979995
kenty <kenta.kgac@gmail.com>
2025-10-17 01:29:28 +0900

    deployagain

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,7 +4,7 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
+  Rails.application.configure do
     config.content_security_policy do |policy|
       policy.default_src :self, :https, :data, :blob
 #     policy.font_src    :self, :https, :data
